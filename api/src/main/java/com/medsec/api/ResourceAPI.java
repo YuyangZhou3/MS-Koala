eeb7254616cff84df0bbaababbe47f4155093eb8
Calvin Lin <62937414+principal-gorilla@users.noreply.github.com>
2020-10-21 09:34:01 +1100

    update resource deleting function (clear space after deletion), add more resource testing files

--- api/src/main/java/com/medsec/api/ResourceAPI.java
+++ api/src/main/java/com/medsec/api/ResourceAPI.java
@@ -5,6 +5,7 @@ import com.medsec.entity.User;
 import com.medsec.filter.Secured;
 import com.medsec.util.Database;
 import com.medsec.util.DefaultRespondEntity;
+import com.medsec.util.TCPServer;
 import com.medsec.util.UserRole;
 import org.glassfish.jersey.server.JSONP;
 
@@ -15,6 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.io.File;
+import java.nio.file.NoSuchFileException;
 import java.util.List;
 
 /**
@@ -87,16 +89,46 @@ public class ResourceAPI {
         Resource resource = db.getResource(resourceID);
         System.out.println(resource.getId());
         if(resource==null){
-            db.close();
             return Response
                     .status(Response.Status.NOT_FOUND)
                     .entity(new DefaultRespondEntity("resource that to be deleted doesn't existed in db"))
                     .build();
         }
+
+        // remove resource file in disk
+        boolean isClear = clearSpace(resource.getContent());
+        if(isClear)
+            System.out.println("Resource pdf file has been removed ");
+        else
+            System.out.println("Resource pdf file deletion failed");
+
         db = new Database();
         db.deleteResource(resourceID);
         db.close();
         return Response.ok(new DefaultRespondEntity()).build();
+    }
+
+    private boolean clearSpace(String filePath){
+        try{
+            String resoucePath = TCPServer.class.getResource("/").getPath();
+            System.out.println("resourcePath: "+resoucePath);
+
+            String webappsDir = (new File(resoucePath,"../../")).getCanonicalPath();
+            System.out.println("webappsDir: "+webappsDir);
+
+            String absoluteFilePath = webappsDir + filePath;
+            System.out.println(absoluteFilePath);
+
+            File file = new File(absoluteFilePath);
+            if(file.exists())
+                file.delete();              // directory won't be removed
+            else
+                throw new NoSuchFileException("File does not exist");
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     // Fetch and return a particular resource pdf file
@@ -127,7 +159,6 @@ public class ResourceAPI {
     }
 
     private List <Resource> retrieveUserResources(String uid) {
-
         Database db = new Database();
         return db.listUserResources(uid);
     }
