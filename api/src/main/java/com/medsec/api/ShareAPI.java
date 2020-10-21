be7d11d2c51c3ac00cf6bd74df96e263fc46e010
Calvin Lin <62937414+principal-gorilla@users.noreply.github.com>
2020-10-21 12:06:51 +1100

    fix mailing bugs

--- api/src/main/java/com/medsec/api/ShareAPI.java
+++ api/src/main/java/com/medsec/api/ShareAPI.java
@@ -33,68 +33,53 @@ public class ShareAPI {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Secured
-    public Response shareAppointment(
-            @Context SecurityContext sc,
-            Mail mail
-    ) {
+    public Response shareAppointment(@Context SecurityContext sc, Mail mail) {
 
         String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
         User requestUser = (User) sc.getUserPrincipal();
+
+        mail = new Mail().id("1").mailto("calvinlin0606@gmail.com").content("testing email");
+        System.out.println(mail.toString());
+
         if(mail.getId()==null)
-        {
             return Response.status(Response.Status.BAD_REQUEST).entity(new DefaultRespondEntity("The appointment id not specified")).build();
-        }
+
         Database db = new Database();
         Appointment app = db.getAppointment(mail.getId());
         db.close();
+
         if (app == null)
             return Response.status(Response.Status.NOT_FOUND).entity(new DefaultRespondEntity("Could not find requested appointment")).build();
-        if(!app.getUid().equals(requestUser.getId()))
-        {
-            return Response.status(Response.Status.FORBIDDEN).entity(new DefaultRespondEntity("Requested appointment for different user")).build();
-        }
 
-        if(!Pattern.matches(REGEX_EMAIL,mail.getMailto()))
-        {
+        if(!app.getUid().equals(requestUser.getId()))
+            return Response.status(Response.Status.FORBIDDEN).entity(new DefaultRespondEntity("Requested appointment for different user")).build();
+
+        if(!Pattern.matches(REGEX_EMAIL, mail.getMailto()))
             return Response.status(Response.Status.BAD_REQUEST).entity("The email address not properly formed").build();
-        }
 
         Gson gson = new Gson();
-
-        // 2. Java object to JSON string
         String appointmentJSON = gson.toJson(app);
+
+        System.out.println(appointmentJSON);
+
+        String textHtml = "";
 
         if (mail.getMailto() != null) {
             String name = mail.getMailto().split("@")[0];
             Email email = new EmailBuilder()
                               .from("Medical heal team", "medical.heal12345@gmail.com")
                               .to(name, mail.getMailto())
-                              .subject("Java Mail Test")
+                              .subject("Sharing Appointment Information")
                               .text(appointmentJSON)
                               .build();
 
-            //new Mailer("smtp.gmail.com", 587, "icu.swen90014@gmail.com", "swen90014",
-             //       TransportStrategy.SMTP_TLS).sendMail(email);
-            new Mailer("smtp.gmail.com", 587, "medical.heal12345@gmail.com", "tttqqqvvv!!!medheal12345%$#@!",
-                                        TransportStrategy.SMTP_TLS).sendMail(email);
-
+            Mailer mailer = new Mailer("smtp.gmail.com", 587, "medical.heal12345@gmail.com", "tttqqqvvv!!!medheal12345%$#@!",
+                                        TransportStrategy.SMTP_TLS);
+            mailer.sendMail(email);
 
             return Response.ok(app).build();
         }
-
-        else {return Response.status(Response.Status.FORBIDDEN).entity("An error occured while sending email").build();}
-
-
-    }
-
-
-
-    @POST
-    @Path("share/cal")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response testCallum(Callum c) {
-
-        return Response.ok(new DefaultRespondEntity("Deb")).build();
+        else
+            return Response.status(Response.Status.FORBIDDEN).entity("An error occured while sending email").build();
     }
 }
